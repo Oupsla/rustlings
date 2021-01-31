0242359f283eca2dd4c29be964a04b908cd90814
Nicolas D <nicolas_delperdange@hotmail.com>
2021-01-31 20:19:28 +0100

    add solutions till enums3

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(number_of_apple: i32) -> i32 {
+    if(number_of_apple >= 40) {
+        number_of_apple * 1
+    } else {
+        number_of_apple * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
